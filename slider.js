28f28cd518125c5fe6db585f8fe260872e8e2174
NatureSL <varistrue@gmail.com>
2023-10-11 00:47:55 +0600

    fix: reinit second slider

--- slider.js
+++ slider.js
@@ -1,34 +1,31 @@
-const sliderSelectorList = ['.slider-colection-one', '.slider-colection-two'];
+const sliderSelectorList = ['.slider-colection-one'];
 const $j = jQuery.noConflict();
 
-$j(document).ready(function () {
-});
-  sliderSelectorList.forEach((sliderSelector) => {
-    $j(sliderSelector).slick({
-      dots: true,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      settings: "unslick",
-      responsive: [
-        {
-          breakpoint: 750,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-          },
+sliderSelectorList.forEach((sliderSelector) => {
+  $j(sliderSelector).slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
         },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
         },
-      ],
-    });
+      },
+    ],
   });
+});
 
 $j(".tab a").on("click", function (e) {
   e.preventDefault();
@@ -39,4 +36,29 @@ $j(".tab a").on("click", function (e) {
 
   $j(".tab-content > div").not(target).hide();
   $j(target).fadeIn(600);
+
+  // Переинициализируем слайдеры Slick перед их отображением
+  $j(target).find(".slider-colection-two").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 });
